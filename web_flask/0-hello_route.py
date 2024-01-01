daaacf2d23ca131a697ef4d6dc9f6d6d418065fd
Heavenboi <nigelzenda98@gmail.com>
2024-01-01 18:53:05 +0200

    added a readme and 0-hello_route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that run a flask app and return Hello HBNB"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """method that displays Hello HBNB!"""
+    return "<p> Hello HBNB! </p>"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
